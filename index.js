8663c02d0e6f6f128158bdc7c22b9ead3c094f87
aaryan <aaryab.ssj@gmail.com>
2021-09-09 12:05:08 +0530

    oye

---
+++ index.js
@@ -0,0 +1,4 @@
+const x=require('express');
+const y=x();
+y.use(x.static('static'));
+y.listen(3000,console.log("The best fighter is never angry. Peacr comes from within. Take note of things you are grateful for to lose sight of the things you lack. "));
